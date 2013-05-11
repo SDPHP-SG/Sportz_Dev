a245b4aeb0e40ace3b7669627b31cb7547342fb8
mattsg <msgjobs@gmail.com>
2013-05-11 15:41:21 -0700

    Added some documentation.  Added the models folder and models/main and models/baskteball folder.  Started adding code for user login and signup.

---
+++ application/models/main/user_model.php
@@ -0,0 +1,13 @@
+<?php
+
+class User_model extends CI_Model {
+
+	public function __construct() {
+		parent::__construct();
+	}
+
+}
+
+/*end of class User_model*/
+
+/* end of file user_model.php */
